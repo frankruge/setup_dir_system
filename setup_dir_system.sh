2c187f9d421c774a981fc05856ca2ec66a1d6ce0
frankruge <frankruge@gmail.com>
2018-04-27 20:06:35 +0200

    Update setup_dir_system.sh

--- setup_dir_system.sh
+++ setup_dir_system.sh
@@ -45,20 +45,13 @@ ln -s $DIR0A/$DIR_1/$DIR_D/results $DIR0B/$DIR_1/$DIR_D/results
 ln -s $DIR0A/$DIR_1/$DIR_E/results $DIR0B/$DIR_1/$DIR_E/results
 
 #####################################
-#copy shell scripts to DIR_A - DIR_E
-#No
+#copy shell and R scripts to DIR_A - DIR_E
+#
 #####################################
 cp $DIR0B/betti_star/data/*.sh $DIR0B/$DIR_1/data/
 cp $DIR0B/betti_star/00*/*.sh $DIR0B/$DIR_1/$DIR_A/
 cp $DIR0B/betti_star/01*/*.sh $DIR0B/$DIR_1/$DIR_B/
 cp $DIR0B/betti_star/02*/*.sh $DIR0B/$DIR_1/$DIR_C/
-#cp $DIR0B/01_star/07*/*.sh $DIR0B/$DIR_1/$DIR_D/
 cp $DIR0B/betti_star/03*/*.R $DIR0B/$DIR_1/$DIR_D/
 cp $DIR0B/betti_star/03*/*.sh $DIR0B/$DIR_1/$DIR_D/
-
-#cp $DIR0B/new_analysis/07*/*.sh $DIR0B/$DIR_1/$DIR_E/
-#cp $DIR0B/new_analysis/07*/*.R $DIR0B/$DIR_1/$DIR_E/
-
-
-#cp $DIR0B/"var3Deseq_analysis"/$DIR_E/*.sh $DIR0B/$DIR_1/$DIR_E/
 
